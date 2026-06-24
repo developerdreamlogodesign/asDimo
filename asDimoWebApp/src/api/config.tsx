a067d4cfee570bbe08fc5089ad1136aaa5489728
developer <developer@dreamlogodesign.net>
2026-06-24 17:51:02 +0530

    url change

--- asDimoWebApp/src/api/config.tsx
+++ asDimoWebApp/src/api/config.tsx
@@ -1,6 +1,6 @@
-export const BASE_URL = "https://dreamgroupsindia.com/dev/asDimoBackend/api";
+export const BASE_URL = "https://dreamgroupsindia.com/dev/asDimo/asDimoBackend/api";
 // export const BASE_URL = "http://localhost:4000/api";
 // export const basename = "/"; 
-export const basename = "/dev/asDimoWebApp"; 
-export const filebasename = "https://dreamgroupsindia.com/dev/asDimoBackend";
+export const basename = "/dev/asDimo/asDimoWebApp"; 
+export const filebasename = "https://dreamgroupsindia.com/dev/asDimo/asDimoBackend";
 
